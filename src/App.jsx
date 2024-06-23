e02d7dca0b6747182499b138515f628ea52964dd
Saksham Khare <sakshamkhare19@gmail.com>
2024-06-23 18:51:42 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 import "./App.css";
+import { Landing } from "./components/Landing";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Dashboard } from "./components/Dashboard";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
