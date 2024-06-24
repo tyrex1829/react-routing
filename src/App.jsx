192d770f664810beac778871df011abbff54d837
Saksham Khare <sakshamkhare19@gmail.com>
2024-06-24 12:42:44 +0530

    starting prop-drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,70 @@
-import React, { Suspense, useState } from "react";
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
-import "./App.css";
-const Landing = React.lazy(() => import("./components/Landing"));
-const Dashboard = React.lazy(() => import("./components/Dashboard"));
+// import React, { Suspense, useState } from "react";
+// import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+// import "./App.css";
+// const Landing = React.lazy(() => import("./components/Landing"));
+// const Dashboard = React.lazy(() => import("./components/Dashboard"));
 
-function App() {
-  const [count, setCount] = useState(0);
+// function App() {
+//   const [count, setCount] = useState(0);
 
-  // router array, generally we map over this array for over routes.
-  // const router = [
-  //   {
-  //     route: "/",
-  //     component: Dashboard,
-  //   },
-  // ];
+// router array, generally we map over this array for over routes.
+// const router = [
+//   {
+//     route: "/",
+//     component: Dashboard,
+//   },
+// ];
 
-  return (
-    <div>
-      <BrowserRouter>
-        <Appbar />
-        <Routes>
-          <Route
-            path="/dashboard"
-            element={
-              <Suspense fallback={"loading..."}>
-                <Dashboard />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/"
-            element={
-              <Suspense fallback={"loading..."}>
-                <Landing />
-              </Suspense>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-}
+// for routing
+//   return (
+//     <div>
+//       <BrowserRouter>
+//         <Appbar />
+//         <Routes>
+//           <Route
+//             path="/dashboard"
+//             element={
+//               <Suspense fallback={"loading..."}>
+//                 <Dashboard />
+//               </Suspense>
+//             }
+//           />
+//           <Route
+//             path="/"
+//             element={
+//               <Suspense fallback={"loading..."}>
+//                 <Landing />
+//               </Suspense>
+//             }
+//           />
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
 
-function Appbar() {
-  const navigate = useNavigate();
+// this code is for routing Appbar
+// function Appbar() {
+//   const navigate = useNavigate();
 
-  return (
-    <div>
-      <button
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        Landing Page
-      </button>
-      <button
-        onClick={() => {
-          navigate("/dashboard");
-        }}
-      >
-        Dashboard Page
-      </button>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {
+//           navigate("/");
+//         }}
+//       >
+//         Landing Page
+//       </button>
+//       <button
+//         onClick={() => {
+//           navigate("/dashboard");
+//         }}
+//       >
+//         Dashboard Page
+//       </button>
+//     </div>
+//   );
+// }
 
 export default App;
