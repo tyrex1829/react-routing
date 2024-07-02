b3ad56d8c4f309ce872db16fb722edc1d4e299ae
tyrex1829 <sakshamkhare19@gmail.com>
2024-07-02 13:36:43 +0530

    repo chnages

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
+// import { createContext } from "react";
+
+// export const CountContext = createContext(0);
+
+// changes for wsl workspace
 import { createContext } from "react";
 
 export const CountContext = createContext(0);
 
 // changes for wsl workspace
-=======
-import { createContext } from "react";
-
-export const CountContext = createContext(0);
-
-// changes for wsl workspace
->>>>>>> 24b15ed501df37a8787d718e0f0a438ed1f43cf7
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
+// <<<<<<< HEAD
+// export default function Landing() {
+//   return <div>Landing Page</div>;
+// }
+// =======
+
 export default function Landing() {
   return <div>Landing Page</div>;
 }
-=======
-export default function Landing() {
-  return <div>Landing Page</div>;
-}
->>>>>>> 24b15ed501df37a8787d718e0f0a438ed1f43cf7
