76133c06baf2be7a1c85a214aaf1f176393141a6
tyrex1829 <sakshamkhare19@gmail.com>
2024-07-02 13:18:46 +0530

    clone repo

---
+++ src/App.jsx
@@ -0,0 +1,189 @@
+// import React, { Suspense, useState } from "react";
+// import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+// import "./App.css";
+// const Landing = React.lazy(() => import("./components/Landing"));
+// const Dashboard = React.lazy(() => import("./components/Dashboard"));
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+// router array, generally we map over this array for over routes.
+// const router = [
+//   {
+//     route: "/",
+//     component: Dashboard,
+//   },
+// ];
+
+// for routing
+//   return (
+//     <div>
+//       <BrowserRouter>
+//         <Appbar />
+//         <Routes>
+//           <Route
+//             path="/dashboard"
+//             element={
+//               <Suspense fallback={"loading..."}>
+//                 <Dashboard />
+//               </Suspense>
+//             }
+//           />
+//           <Route
+//             path="/"
+//             element={
+//               <Suspense fallback={"loading..."}>
+//                 <Landing />
+//               </Suspense>
+//             }
+//           />
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// this code is for routing Appbar
+// function Appbar() {
+//   const navigate = useNavigate();
+
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {
+//           navigate("/");
+//         }}
+//       >
+//         Landing Page
+//       </button>
+//       <button
+//         onClick={() => {
+//           navigate("/dashboard");
+//         }}
+//       >
+//         Dashboard Page
+//       </button>
+//     </div>
+//   );
+// }
+
+// code for prop-drilling & context api
+
+// import { useContext, useState } from "react";
+// import "./App.css";
+// import { CountContext } from "./Context";
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <CountContext.Provider value={{ count, setCount }}>
+//         <Count />
+//       </CountContext.Provider>
+//     </div>
+//   );
+// }
+
+// function Count() {
+//   return (
+//     <div>
+//       <CountRenderer />
+//       <Buttons />
+//     </div>
+//   );
+// }
+
+// function CountRenderer() {
+//   const { count } = useContext(CountContext);
+//   return <div>{count}</div>;
+// }
+
+// function Buttons() {
+//   const { count, setCount } = useContext(CountContext);
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {
+//           setCount(count + 1);
+//         }}
+//       >
+//         increment
+//       </button>
+//       <button
+//         onClick={() => {
+//           setCount(count - 1);
+//         }}
+//       >
+//         decrement
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// changes for wsl workspace
+
+//
+//
+// code for recoil(changes in context code).
+
+import { useContext } from "react";
+import "./App.css";
+import { CountContext } from "./Context";
+import {
+  RecoilRoot,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
+import { countAtom } from "./store/atoms/count";
+
+function App() {
+  return (
+    <RecoilRoot>
+      <Count />
+    </RecoilRoot>
+  );
+}
+
+function Count() {
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons />
+    </div>
+  );
+}
+
+function CountRenderer() {
+  const count = useRecoilValue(countAtom);
+
+  return <div>{count}</div>;
+}
+
+function Buttons() {
+  const setCount = useSetRecoilState(countAtom);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount((count) => count + 1);
+        }}
+      >
+        increment
+      </button>
+      <button
+        onClick={() => {
+          setCount((count) => count - 1);
+        }}
+      >
+        decrement
+      </button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Context.jsx
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+export const CountContext = createContext(0);
+
+// changes for wsl workspace
